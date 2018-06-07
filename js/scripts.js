28028f2be731278527db47bdafc973dc42115c09
Brandon Betts and Renee Sarley <brandon@>
2018-06-07 12:37:02 -0700

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -10,41 +10,43 @@ function toRoman(number) {
   var firstArray = []
   var finalNumber = ''
 
+// Conditions for determining what kind / how big the number is
+
   if (number < 10) {
     one = 'I'.repeat(number)
     firstArray = [one]
-    alert(firstArray)
+    // alert(firstArray)
   } else if (number < 100) {
     ten = 'X'.repeat(parseInt(numbers[0]))
     one = 'I'.repeat(parseInt(numbers[1]))
     firstArray = [ten, one]
-    alert(firstArray)
+    // alert(firstArray)
   } else if (number >= 100 && number < 1000) {
     hundred = 'C'.repeat(parseInt(numbers[0]))
     ten = 'X'.repeat(parseInt(numbers[1]))
     one = 'I'.repeat(parseInt(numbers[2]))
     firstArray = [hundred, ten, one]
-    alert(firstArray)
+    // alert(firstArray)
   } else if (number >= 1000 && number < 3999) {
     thousand = 'M'.repeat(parseInt(numbers[0]))
     hundred = 'C'.repeat(parseInt(numbers[1]))
     ten = 'X'.repeat(parseInt(numbers[2]))
     one = 'I'.repeat(parseInt(numbers[3]))
     firstArray = [thousand, hundred, ten, one]
-    alert(firstArray)
+    // alert(firstArray)
   } else {
     alert("Please enter a number less than 4000.")
     return
   }
 
-    // firstArray = firstArray.split('')
+// Loop for converting the number into roman numerals
 
   for (i = 0; i < firstArray.length; i++) {
     var stringPositionIIIII = (firstArray[i]).search("IIIII")
     if(stringPositionIIIII >= 0){
       newElement = firstArray[i].replace('IIIII', 'V')
       firstArray.splice(i,1,newElement);
-      alert("our element is now this: " + newElement);
+      // alert("our element is now this: " + newElement);
 
     } else {
       console.log("passed")
@@ -54,7 +56,7 @@ function toRoman(number) {
     if(stringPositionIIII >= 0){
       newElement = firstArray[i].replace('IIII', 'IV')
       firstArray.splice(i,1,newElement);
-      alert("our element is now this: " + newElement);
+      // alert("our element is now this: " + newElement);
 
     } else {
       console.log("passed")
@@ -64,7 +66,7 @@ function toRoman(number) {
     if(stringPositionXXXXX >= 0){
       newElement = firstArray[i].replace('XXXXX', 'L')
       firstArray.splice(i,1,newElement);
-      alert("our element is now this: " + newElement);
+      // alert("our element is now this: " + newElement);
 
     } else {
       console.log("passed")
@@ -82,7 +84,7 @@ function toRoman(number) {
     if(stringPositionCCCCC >= 0){
       newElement = firstArray[i].replace('CCCCC', 'D')
       firstArray.splice(i,1,newElement);
-      alert("our element is now this: " + newElement);
+      // alert("our element is now this: " + newElement);
 
     } else {
       console.log("passed")
@@ -91,7 +93,7 @@ function toRoman(number) {
     if(stringPositionCCCC >= 0){
       newElement = firstArray[i].replace('CCCC', 'CD')
       firstArray.splice(i,1,newElement);
-      alert("our element is now this: " + newElement);
+      // alert("our element is now this: " + newElement);
 
     } else {
       console.log("passed")
@@ -111,7 +113,7 @@ $(document).ready(function() {
     event.preventDefault();
 
     var ogNumber = $('input#ogNumber').val();
-    alert(ogNumber);
+    // alert(ogNumber);
 
     toRoman(ogNumber);
 
