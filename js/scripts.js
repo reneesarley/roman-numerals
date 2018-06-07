68d4d92fe417639de529f78aa09b6b547c539c2c
Brandon Betts and Renee Sarley <brandon@>
2018-06-07 09:56:05 -0700

    add first four scripts for 1s, 10s, 100s, and 1000s roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 // business logic
 
 function toRoman(number) {
-  return 'I'.repeat(number);
+
+  var numbers = number.toString().split('');
+
+  if (number < 10) {
+    return 'I'.repeat(number)
+  } else if (number < 100) {
+    return 'X'.repeat(parseInt(numbers[0]))
+  } else if (number > 100 && number < 1000) {
+    return 'C'.repeat(parseInt(numbers[0]))
+  } else if (number >= 1000 || number < 3999) {
+    return 'M'.repeat(parseInt(numbers[0]))
+  } else {
+    alert("Please enter a number.")
+    return
+  }
+  alert(numbers)
 }
+
 
 // user logic
